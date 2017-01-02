a42f24c7d11df296abfc2f443cc6a650e32db1ed
Rafael Dutra <rafael@du3x.com>
2017-01-02 13:55:46 -0200

    Build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ATLAS_TOKEN="EZlMdWy1fAMsbg.atlasv1.gj6rtP6lvW7fUcuCZpW9URTaii7xF57zWMTsyG7yBuzz9WZakqUayDbpWjE9zSvz3yk"
-curl https://atlas.hashicorp.com/api/v1/boxes \
-    -X POST \
-    -H "X-Atlas-Token: $ATLAS_TOKEN" \
-    -d box[name]="raffaeldutra/debian"
+set -e
+
+service=${1}
+
+./packer.exe build -force services/${service}.json
