f2cd790834b5997242df9a8c134d13a1026706e0
Rafael Dutra <raffaeldutra@gmail.com>
2017-01-09 14:58:26 -0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ATLAS_TOKEN="EZlMdWy1fAMsbg.atlasv1.gj6rtP6lvW7fUcuCZpW9URTaii7xF57zWMTsyG7yBuzz9WZakqUayDbpWjE9zSvz3yk"
-curl https://atlas.hashicorp.com/api/v1/boxes \
-    -X POST \
-    -H "X-Atlas-Token: $ATLAS_TOKEN" \
-    -d box[name]="raffaeldutra/debian"
